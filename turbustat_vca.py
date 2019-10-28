add17d5152a5f814e78d5bf949f40a6cb4112eb1
nickill <nicholas.killerby-smith@anu.edu.au>
2019-10-28 19:13:19 +1100

    removed duplicate of turbustat_vca.py and added saving figure to remaining

--- turbustat_vca.py
+++ turbustat_vca.py
@@ -94,7 +94,7 @@ if taper==True:
 
 
 ########################
-def do_vca(vcacube, array_save_loc):
+def do_vca(vcacube, array_save_loc, fig_save_loc):
     """This function greets to
     the person passed in as
     parameter"""
@@ -117,7 +117,7 @@ def do_vca(vcacube, array_save_loc):
         downsamp_vcacube = vcacube.downsample_axis(i, axis=0)
         downsamp_vcacube.allow_huge_operations=True
         vca = VCA(downsamp_vcacube)
-        vca.run(verbose=False, beam_correct=correctbeam)
+        vca.run(verbose=False, beam_correct=correctbeam, save_name=fig_save_loc)
         vca_array=np.vstack((vca_array,[vca.slope,i,vca.slope_err]))
     vca_array=vca_array[1:,:]
 
@@ -127,8 +127,9 @@ def do_vca(vcacube, array_save_loc):
 for j in np.arange(0,7):
 	for i in np.arange(0,7):
 		cube = SpectralCube.read('/avatar/nickill/smc/grid_cubes/smc_grid7x7_x'+str(i)+'_y'+str(j)+'.fits')
-		figsaveloc = '/priv/myrtle1/gaskap/nickill/smc/vca/turbustatoutput/smc_grid7x7_x'+str(i)+'_y'+str(j)
-		do_vca(cube,figsaveloc)                
+		arrayloc = '/priv/myrtle1/gaskap/nickill/smc/vca/turbustatoutput/smc_grid7x7_x'+str(i)+'_y'+str(j)
+		figloc = '/priv/myrtle1/gaskap/nickill/smc/vca/turbustatoutput/smc_grid7x7_x'+str(i)+'_y'+str(j)+'.png'
+		do_vca(cube,arrayloc,figloc)                
 		print('done x'+str(i)+' y'+str(j))
 
 
